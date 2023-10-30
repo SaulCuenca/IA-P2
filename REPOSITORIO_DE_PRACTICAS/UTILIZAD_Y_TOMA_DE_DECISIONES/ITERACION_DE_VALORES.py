f99cbabe2c02c24271a09ce0a8b79367555ffdd3
SaulCuenca <126286987+SaulCuenca@users.noreply.github.com>
2023-10-29 20:07:06 -0600

    Add files via upload

---
+++ REPOSITORIO_DE_PRACTICAS/UTILIZAD_Y_TOMA_DE_DECISIONES/ITERACION_DE_VALORES.py
@@ -0,0 +1,9 @@
+# Lista de valores
+valores = [1, 2, 3, 4, 5]
+
+# Iteración a traves de los valores
+for valor in valores:
+    print(valor)
+    #En este ejemplo, la lista valores contiene números del 1 al 5. El bucle for itera a través de cada elemento de la lista y lo imprime en la consola.
+
+#Si necesitas una iteración más específica o tienes otros requisitos, por favor proporciona más detalles para que pueda ayudarte de manera más precisa.
